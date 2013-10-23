37b766fab11cc06553b1945b76198a37fda77510
Derek Bertubin <dbertubin@fullsail.edu>
2013-10-23 14:03:40 -0400

    Added Project : Birthdat Tracker

---
+++ BirthdayTracker/src/com/petrockz/birthdaytracker/MainActivity.java
@@ -0,0 +1,46 @@
+package com.petrockz.birthdaytracker;
+
+
+
+
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.view.Menu;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+@SuppressLint("SetJavaScriptEnabled")
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.webview);
+		
+		WebView webView = (WebView) findViewById(R.id.webView);
+		webView.loadUrl("file:///android_asset/index.html");
+		webView.setWebViewClient(new ThisWebViewClient());
+		
+			WebSettings webSettings = webView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	private class ThisWebViewClient extends WebViewClient {
+
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView webview, String url) {
+			webview.loadUrl(url);
+			return true;
+		}
+	}
+}
